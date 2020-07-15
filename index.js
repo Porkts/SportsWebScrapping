e11f2f0ca5e85f52d000b609e2366f226a20dd90
Everton Recchi <porktss@gmail.com>
2020-07-14 22:42:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const URL_TO_SCRAP = 'https://globoesporte.globo.com/futebol/brasileirao-serie-a/'
+
+// Fazendo a requisicao a pagina
+axios.get(URL_TO_SCRAP)
+.then(response => {
+
+    // carregando a resposta da requisica para um objeto do cheerio
+    const $ = cheerio.load(response.data)
+
+    eval($('#scriptReact').html().split('const').join('var'))
+    console.log(listaJogos)
+})
